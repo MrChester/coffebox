4247fa4e57ea64d7ea159084be908c41c9b28264
Yauheni Ramankou <iromankov2010@gmail.com>
2021-09-30 14:57:57 +0300

    chore: add vars

--- coffeebox.cpp
+++ coffeebox.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
+#define ESPRESSO_PRICE 1.8
+#define CAPUCCINO_PRICE 2.3
+#define LATTE_PRICE 1.8
+#define ESPRESSO_MENU_NUMBER 2
+#define CAPUCCINO_MENU_NUMBER 3
+#define LATTE_MENU_NUMBER 4
+#define PROGRESS_BAR_WIDTH 20
+#define PROGRESS_BAR_STEP 0.10
+#define PROGRESS_BAR_MAX_VALUE 100.0
+
 int main()
 {
+  double balance = 0.00;
 
   cout << "ok";
   return 0;
